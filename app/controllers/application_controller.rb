99da79d7b571747371ee81cf2e3719f02e52513e
Gregory Losco <gtlosco@gmail.com>
2020-01-25 16:34:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,10 @@ class App < Sinatra::Base
     post '/teams' do 
       @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
       
-      params[:team][:member].each do |member|
-        Member.new(member)
+      members = params[:team][:members]
+      @super_heroes = members.collect do |member_params|
+        SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
       end
-      
-      @members = Member.all
       
       erb :team
       
